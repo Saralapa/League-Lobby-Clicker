b9a548a309fb7ad12e7eea8e86801cc79e5b30fd
Saralapa <saralapagamer@gmail.com>
2024-04-17 02:04:51 -0300

    Removido # de uma linha, o que estava a deixando como comentário

--- utils/Janela.py
+++ utils/Janela.py
@@ -60,7 +60,7 @@ def tela_selecao_de_modo():
         while True:
             if jogo_está_aberto == False:
                 return
-            #time.sleep(30)
+            time.sleep(30)
             if not [window for window in gw.getWindowsWithTitle("League of Legends (TM) Client") if window.title == "League of Legends (TM) Client"] and jogo_está_aberto == True:
                 if idioma == "Portugues":
                     modo_de_jogo = "Apenas auto aceitar"
